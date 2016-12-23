2c5ff7cad8895f4f594e26d575a371c8e2b9a5e0
David DeWinter <david.dewinter@gmail.com>
2016-12-23 07:06:22 -0600

    Google Maps on page centered on Tiger Mt

---
+++ app/components/app.component.js
@@ -0,0 +1,54 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var ngc = require('@angular/core');
+var AppComponent = (function () {
+    function AppComponent() {
+        var _this = this;
+        window['initMap'] = function () {
+            _this.mapElementRef.nativeElement = new google.maps.Map(_this.mapElementRef.nativeElement, {
+                center: { lat: 47.4883, lng: -121.9467 },
+                zoom: 12
+            });
+        };
+    }
+    AppComponent.prototype.ngAfterViewInit = function () {
+        this.fillMapToClientBounds();
+        var node = document.createElement('script');
+        node.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDuUvDZsuhwVYScoNpK6Id4SEC3jQ73YSI&callback=initMap';
+        node.type = 'text/javascript';
+        document.getElementsByTagName('head')[0].appendChild(node);
+    };
+    AppComponent.prototype.fillMapToClientBounds = function () {
+        var mapElement = this.mapContainerElementRef.nativeElement;
+        var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var height = Math.min(document.documentElement.clientHeight, window.innerHeight || 0);
+        mapElement.style.width = width;
+        mapElement.style.height = height;
+    };
+    __decorate([
+        ngc.ViewChild('mapContainer'), 
+        __metadata('design:type', ngc.ElementRef)
+    ], AppComponent.prototype, "mapContainerElementRef", void 0);
+    __decorate([
+        ngc.ViewChild('map'), 
+        __metadata('design:type', ngc.ElementRef)
+    ], AppComponent.prototype, "mapElementRef", void 0);
+    AppComponent = __decorate([
+        ngc.Component({
+            selector: 'solo-tracker',
+            templateUrl: 'app/components/app.component.html',
+        }), 
+        __metadata('design:paramtypes', [])
+    ], AppComponent);
+    return AppComponent;
+}());
+exports.AppComponent = AppComponent;
+//# sourceMappingURL=app.component.js.map
--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,9 +1,37 @@
-import { Component }                 from '@angular/core';
+import * as ngc from '@angular/core';
 
-@Component({
-    selector: 'my-app',
+@ngc.Component({
+    selector: 'solo-tracker',
     templateUrl: 'app/components/app.component.html',
 })
-export class AppComponent {
-    
+export class AppComponent implements ngc.AfterViewInit {
+    @ngc.ViewChild('mapContainer') mapContainerElementRef: ngc.ElementRef
+    @ngc.ViewChild('map') mapElementRef: ngc.ElementRef
+
+    constructor() {
+        window['initMap'] = () => {
+            this.mapElementRef.nativeElement = new google.maps.Map(this.mapElementRef.nativeElement, {
+                center: { lat: 47.4883, lng: -121.9467 },
+                zoom: 12
+            });
+        };
+    }
+
+    ngAfterViewInit(): void {
+        this.fillMapToClientBounds();
+
+        let node = document.createElement('script');
+        node.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDuUvDZsuhwVYScoNpK6Id4SEC3jQ73YSI&callback=initMap';
+        node.type = 'text/javascript';
+        document.getElementsByTagName('head')[0].appendChild(node);
+    }
+
+    fillMapToClientBounds(): void {
+        let mapElement = this.mapContainerElementRef.nativeElement;
+        let width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        let height = Math.min(document.documentElement.clientHeight, window.innerHeight || 0);
+
+        mapElement.style.width = width;
+        mapElement.style.height = height;
+    }
 }
